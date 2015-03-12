68f12bd0656dcd7ca413974497f26e7049dd83cc
Caleb Durenberger <caleb.durenberger@moody.edu>
2015-03-12 14:56:59 -0500

    added privacy policy and fixed a couple css issues

--- routes/pages.js
+++ routes/pages.js
@@ -7,5 +7,11 @@ module.exports = function (app) {
 
   app.get("/", home);
   app.get("/index", home);
-};
+  app.get("/privacy-policy", function (req, res) {
+    res.render('privacy-policy', {
+      some: "variables",
+      here: "foo"
+    });
+  });
 
+};
