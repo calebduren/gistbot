5f958a318ecf5278fd7be8726e02a0a796a14161
Caleb Durenberger <calebdurenberger@gmail.com>
2015-04-20 20:49:00 -0500

    changed the email in footer for security

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 function revealEmail() {
   $("#email").html('hi@gistbot.com');
   setTimeout(function () {
-    $("#email").attr('href', 'mailto:hi@gistbot.com');
+    $("#email").attr('href', 'mailto:&#104;&#105;&#64;&#103;&#105;&#115;&#116;&#98;&#111;&#116;&#46;&#99;&#111;&#109;');
   }, 50);
 }
 
