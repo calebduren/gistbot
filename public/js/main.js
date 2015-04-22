f10b4ea1f743eb953a1cadbafb621f853e7df98b
Caleb Durenberger <calebdurenberger@gmail.com>
2015-04-21 23:03:34 -0500

    updated placeholder text

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,11 @@
-function revealEmail() {
-  $("#email").html('&#104;&#105;&#64;&#103;&#105;&#115;&#116;&#98;&#111;&#116;&#46;&#99;&#111;&#109;');
-  setTimeout(function () {
-    $("#email").attr('href', 'mailto:hi@gistbot.com');
-  }, 50);
-}
-
-if ($(window).width() < 768) {
-  $("#getGist").attr('placeholder', 'i.e. Nitrogen or goo.gl/zWz1jU');
-}
+$(window).load(function(){
+  function revealEmail() {
+    $("#email").html('&#104;&#105;&#64;&#103;&#105;&#115;&#116;&#98;&#111;&#116;&#46;&#99;&#111;&#109;');
+    setTimeout(function () {
+      $("#email").attr('href', 'mailto:hi@gistbot.com');
+    }, 50);
+  }
+});
 
 $(window).scroll(function(){
   var scrollDist = $(window).scrollTop(),
