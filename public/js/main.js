bd96215be056e7b4fe74d778b1ab7d89983f3dbe
Caleb Durenberger <calebdurenberger@gmail.com>
2015-04-22 00:01:50 -0500

    logo animation on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,30 @@
-$(window).load(function(){
-  function revealEmail() {
-    $("#email").html('&#104;&#105;&#64;&#103;&#105;&#115;&#116;&#98;&#111;&#116;&#46;&#99;&#111;&#109;');
-    setTimeout(function () {
-      $("#email").attr('href', 'mailto:hi@gistbot.com');
-    }, 50);
-  }
-});
 
-$(window).scroll(function(){
+function revealEmail() {
+  $("#email").html('&#104;&#105;&#64;&#103;&#105;&#115;&#116;&#98;&#111;&#116;&#46;&#99;&#111;&#109;');
+  setTimeout(function () {
+    $("#email").attr('href', 'mailto:hi@gistbot.com');
+  }, 50);
+}
+
+
+$(window).on("load resize scroll",function(e){
   var scrollDist = $(window).scrollTop(),
       brandBar = $('#brandBar').height(),
-      lightHeight = $('#header').height() - brandBar - 20,
-      darkHeight = $('#header').height() + brandBar - 20,
+      lightHeight = $('#header').height() - brandBar - 30,
+      darkHeight = $('#header').height() + brandBar - 30,
       logoLight = $('#logo-light'),
       logoDark = $('#logo-dark');
 
   if ( scrollDist > lightHeight ) {
-    logoLight.stop().css({'display': 'none'});
+    logoLight.addClass('top');
   } else {
-    logoLight.stop().css({'display': 'inline-block'});
+    logoLight.removeClass('top');
   }
 
   if ( scrollDist > darkHeight ) {
-    logoDark.stop().css({'display': 'inline-block'});
+    logoDark.removeClass('left');
   } else {
-    logoDark.stop().css({'display': 'none'});
+    logoDark.addClass('left');
   }
 
 });
