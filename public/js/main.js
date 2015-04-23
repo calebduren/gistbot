e955a15b5c1ea2df36c1aa24e0a0b159e80d78bc
Caleb Durenberger <caleb.durenberger@moody.edu>
2015-04-23 13:59:30 -0500

    intro text styling

--- public/js/main.js
+++ public/js/main.js
@@ -3,22 +3,27 @@ jQuery.extend(jQuery.easing, {
   easeInBack: function(x, t, b, c, d, s) {
     if (s == undefined) s = 1.70158;
     return c * (t /= d) * t * ((s + 1) * t - s) + b;
+  },
+  easeOutCirc: function(x, t, b, c, d) {
+    return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
   }
 });
 // End jQuery Easing
 
 // Remove the helper text box after first visit
 
-if (! localStorage.noFirstVisit) {
-    setTimeout(function() {
-      $('#welcome').animate({
-        'opacity': '1'
-      }, 600);
-    }, 800);
-    localStorage.noFirstVisit = "1";
-} else {
-  $('#welcome').remove();
-}
+$(document).ready( function() {
+  var welcome = $('#welcome'),
+      close = $('#close'),
+      footer = $('#footer');
+
+if (!localStorage.noFirstVisit) {
+    welcome.delay(800).animate({'opacity': '1'}, 600);
+  localStorage.noFirstVisit = "1";
+  } else {
+    welcome.delay(500).slideUp(500, 'easeOutCirc');
+  }
+});
 
 function revealEmail() {
   $("#email").html('&#104;&#105;&#64;&#103;&#105;&#115;&#116;&#98;&#111;&#116;&#46;&#99;&#111;&#109;');
@@ -28,16 +33,16 @@ function revealEmail() {
 }
 
 $(window).on("load resize", function() {
-  $('#close').click(function() {
-    // $('#footer').animate({
-    //   'margin-bottom': '-100px'
-    // }, 100);
 
-    $('#footer').slideUp(250, 'easeInBack');
+  var welcome = $('#welcome'),
+    close = $('#close'),
+    footer = $('#footer');
+
+  close.click(function() {
+    footer.slideUp(250, 'easeInBack');
   });
-
-  $('#welcome').click(function() {
-    $('#welcome').slideUp(250, 'easeInBack');
+  welcome.click(function() {
+    welcome.slideUp(250, 'easeInBack');
   });
 });
 
